e554515cc9afe8fe527a8e4753d80690d0d4a046
Ilnur Garifullin <ilnur.email@gmail.com>
2019-02-07 21:13:32 +0300

    Fix uploading path

--- examples/mnist/03-upload/execute.sh
+++ examples/mnist/03-upload/execute.sh
@@ -1,3 +1,6 @@
 hs cluster add --name $CLUSTER_NAME --server $CLUSTER_ADDRESS
 hs cluster use $CLUSTER_NAME
+
+cd ${MNIST_MODELS_DIR}
+cd $(ls -t | head -n1)
 hs upload --name $MODEL_NAME | tail -n 1 | jq ".version" > /model_version.txt
