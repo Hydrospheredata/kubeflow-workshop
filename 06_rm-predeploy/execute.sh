82225c25dbe0c6cee988a202833883c287474fa4
Ilnur Garifullin <ilnur.email@gmail.com>
2019-04-23 21:05:06 +0300

    Remove rm-predeploy

--- 06_rm-predeploy/execute.sh
+++
@@ -1,9 +0,0 @@
-# Define where Hydrosphere Serving instance is running.
-hs cluster add --name serving --server $CLUSTER_ADDRESS
-hs cluster use serving
-
-# Get predeploy application name
-export APPLICATION_NAME=$1
-
-# Remove predeploy application
-hs app rm ${APPLICATION_NAME}
